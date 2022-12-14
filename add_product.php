c78f2f5f03cde8c3f9efea8b2de5dca97f0af8d6
danylo454 <ilchuk.danilo@gmail.com>
2022-12-14 12:17:10 +0200

    Init Project

---
+++ add_product.php
@@ -0,0 +1,107 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $name = $_POST['nameProduct'];
+    $price = $_POST['priceProduct'];
+    $description = $_POST['descriptionProducts'];
+    $images = $_POST['imgProduct'];
+    echo $images;
+    exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Додати Товар</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/addProductStyle.css">
+</head>
+<body>
+<?php include "_header.php"; ?>
+
+<div class="wrapper">
+
+    <form class="p-3 mt-3" enctype="multipart/form-data" method="post">
+
+        <div class="row">
+            <div class="col-12 logo">
+                <img class="logoImg"
+                     id="imgAvatar"
+                     src="https://cdn-irec.r-99.com/sites/default/files/imagecache/300o/product-images/896767/MwbBNcvG9f2sxDif7MZbjQ.jpeg"
+                     alt="">
+                <h1>Додати Товар</h1>
+            </div>
+        </div>
+
+        <div class="custom__form">
+            <div class="custom__image-container">
+                <label id="add-img-label" for="add-single-img">Додати фото </label>
+                <input type="file" id="add-single-img" accept="image/jpeg"/>
+            </div>
+            <input
+                    type="file"
+                    id="image-input"
+                    name="photos"
+                    accept="image/jpeg"
+                    multiple
+            />
+            <br/>
+        </div>
+
+
+
+        <div class="displeyFlex_Space_Bettwen">
+
+            <div class="form-field">
+                <input type="text" name="nameProduct" id="nameProduct" placeholder="Назва Товару">
+            </div>
+
+            <div class="form-field d-flex align-items-center">
+                <input type="number" name="priceProduct" id="priceProduct" placeholder="Ціна">
+            </div>
+        </div>
+        <textarea name="descriptionProducts" id="descriptionProducts" placeholder="Опис Товару"
+                  class="form-field_description" cols='120' rows='6'></textarea>
+        <button class="btn mt-3">Додати Товар</button>
+    </form>
+
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script>
+    // Global variables
+    const imgInputHelper = document.getElementById("add-single-img");
+    const imgInputHelperLabel = document.getElementById("add-img-label");
+    const imgContainer = document.querySelector(".custom__image-container");
+    const imgFiles = [];
+
+    const addImgHandler = () => {
+        const file = imgInputHelper.files[0];
+        if (!file) return;
+        // Generate img preview
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        console.log(file.name);
+        reader.onload = () => {
+            const newImg = document.createElement("img");
+            newImg.id = "imgProduct";
+            newImg.src = reader.result;
+            imgContainer.insertBefore(newImg, imgInputHelperLabel);
+        };
+        // Store img file
+        imgFiles.push(file);
+        // Reset image input
+        imgInputHelper.value = "";
+        return;
+    };
+
+
+    imgInputHelper.addEventListener("change", addImgHandler);
+
+
+</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Головна сторінка</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php include "_header.php"; ?>
+
+<?php
+    include_once "connection_database.php";
+?>
+
+
+<div class="container">
+    <h1 class="text-center">Список продуктів</h1>
+    <section style="background-color: #eee;">
+        <div class="container py-5">
+            <div class="row">
+                <?php
+                $sql = "SELECT * FROM tbl_products";
+                foreach($dbh->query($sql) as $row) {
+                    $id = $row['id'];
+                    $image = $row['image'];
+                    $name = $row["name"];
+                    $price = $row["price"];
+                    //echo "<h1>$id</h1>";
+                    echo '
+                <div class="col-md-6 col-lg-4 mb-4 mb-md-0">
+                    <div class="card">
+                        <img src="images/' . $image . '"
+                             class="card-img-top" alt="Клавіатура"/>
+                        <div class="card-body">
+
+                            <div class="d-flex justify-content-between mb-3">
+                                <h5 class="mb-0">' . $name . '</h5>
+                            </div>
+
+                            <div class="mb-2 d-flex justify-content-between">
+                                <h5 class="text-dark mb-0">' . $price . '₴</h5>
+                                <button type="button" class="btn btn-success">Купить</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                    ';
+                }
+                ?>
+
+
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
